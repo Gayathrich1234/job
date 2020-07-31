c29cddabd83678e528508e7833ee86735c1543ba
Gayathrich1234 <69039756+Gayathrich1234@users.noreply.github.com>
2020-07-31 19:20:39 +0530

    Update Program.sh

--- Program.sh
+++ Program.sh
@@ -1,1 +1,7 @@
-
+cd
+mkdir my-first -program
+cd-my-first-program
+echo "welcome to My first program " >readme.txt
+git commit -m "added readme.txt"
+echo "Learning Git is going well so far." >> readme.txt
+git commit -m "added two files"
